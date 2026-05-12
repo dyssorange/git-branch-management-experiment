6c4d4d3764cc971880fc04d1515177200f7e1d5d
dyss <dyssorange@163.com>
2026-05-13 00:39:18 +0800

    Create utils.c

---
+++ src/src/utils.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "utils.h"
+
+void print_project_info() {
+    printf("Software Engineering Git Experiment\n");
+    printf("This project is used for branch management practice.\n");
+}
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
